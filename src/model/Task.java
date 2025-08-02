683b4b3c77092b7f49fc590d8081ea29f3da4e6f
Konstantin <tenenbaum1994@gmail.com>
2025-08-02 11:01:31 +0700

    Изменение класса Task добавлены поля startTime и duration изменены конструкторы с учетом новых полей добавлены геттеры и сеттеры на новые поля добавлен метод getEndTime() изменен toString()

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Task {
@@ -8,20 +10,26 @@ public class Task {
     private String name;
     private String description;
     private TaskStatus status;
+    private LocalDateTime startTime;
+    private Duration duration;
 
-    public Task(int id, String name, String description, TaskStatus status) {
+    //оставил 4 конструктора на случаи, когда id не задается или не указываются параметры времени
+    public Task(int id, String name, String description,
+                TaskStatus status, LocalDateTime startTime, Duration duration) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.status = status;
+        this.startTime = startTime;
+        this.duration = duration;
     }
 
-    public Task(String name, String description) {
-        this(-1, name, description, TaskStatus.NEW);
+    public Task(int id, String name, String description, TaskStatus status) {
+        this(id, name, description, status, null, Duration.ZERO);
     }
 
-    public Task(int id, String name, String description) {
-        this(id, name, description, TaskStatus.NEW);
+    public Task(String name, String description, TaskStatus status, LocalDateTime startTime, Duration duration) {
+        this(-1, name, description, status, startTime, duration);
     }
 
     public Task(String name, String description, TaskStatus status) {
@@ -32,37 +40,62 @@ public class Task {
         return id;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
     public String getName() {
         return name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public TaskStatus getStatus() {
         return status;
     }
 
+    public TaskType getTaskType() {
+        return TaskType.TASK;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public LocalDateTime getEndTime() {
+        if (startTime == null) {
+            return null;
+        }
+        return startTime.plus(duration);
+    }
+
+
     public void setId(int id) {
         this.id = id;
-    }
-
-    public void setStatus(TaskStatus status) {
-        this.status = status;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public TaskType getTaskType() {
-        return TaskType.TASK;
+    public void setName(String name) {
+        this.name = name;
     }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
+
 
     @Override
     public boolean equals(Object o) {
@@ -78,11 +111,15 @@ public class Task {
 
     @Override
     public String toString() {
-        return String.format("%d,%s,%s,%s,%s,",
+        String startTimeToString = startTime != null ? startTime.toString() : " ";
+        long durationToString = duration != Duration.ZERO ? duration.toMinutes() : 0;
+        return String.format("%d,%s,%s,%s,%s,%s,%d",
                 id,
                 TaskType.TASK,
                 name,
                 description,
-                status);
+                status,
+                startTimeToString,
+                durationToString);
     }
 }
